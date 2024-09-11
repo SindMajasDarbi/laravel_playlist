faea40ce120cba284d111bf184562742b025945b
unknown <your@email.example>
2024-09-11 16:29:08 +0300

    eh

---
+++ app/Models/Playlist.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Playlist extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'tag',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function meta(){
+        return $this->hasOne(PostMeta::class);
+    }
+    public function songs(){
+        return $this->belongsToMany(Song::class);
+    }
+}
+
---
+++ database/migrations/2024_09_11_111024_create_playlist_song_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('playlist_song', function (Blueprint $table) {
+            $table->unsignedBigInteger('song_id');
+            $table->unsignedBigInteger('playlist_id');
+
+            $table->foreign('song_id')->references()->on('songs')->onDelete('cascade');
+            $table->foreign('playlist_id')->references()->on('playlists')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('playlist_song');
+    }
+};
